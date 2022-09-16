9b25893057962033e7fcb43220f22c817e3ee66d
Austin Rhodes <austinrhodes462@gmail.com>
2022-09-15 18:46:53 -0700

    "added trigger phrases"

--- start_HHGTTG_bot.py
+++ start_HHGTTG_bot.py
@@ -4,7 +4,7 @@ from functions import scan_comments
 
 
 trigger_dictionary = {
-    ("so long", ): ["So long, and thanks for all the fish!! (https://www.youtube.com/watch?v=N_dUmDBfp6k)"],
+    ("so long", "thanks for the fish", "all the fish"): ["So long, and thanks for all the fish!! (https://www.youtube.com/watch?v=N_dUmDBfp6k)"],
     ("There is a theory",): ["There is a theory which states that if ever anyone discovers exactly what the Universe is for and why it is here, it will instantly disappear and be replaced by something even more bizarre and inexplicable. There is another theory which states that this has already happened."],
     ("It is a mistake", "potatoes"): ["It is a mistake to think you can solve any major problems just with potatoes."],
     ("new yorkers",): ["The last time anybody made a list of the top hundred character attributes of New Yorkers, common sense snuck in at number 79."],
